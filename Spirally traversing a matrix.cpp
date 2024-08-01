d52c49951f7a77504e09be464d8cf3250048ddb1
Bambam Thakur <131291624+Shivam-thakur05@users.noreply.github.com>
2024-08-01 20:04:54 +0530

    Create Spirally traversing a matrix.cpp

---
+++ Spirally traversing a matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    vector<int> spirallyTraverse(vector<vector<int> > &matrix) {
+        vector<int>ans;
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int totalDigit = row*col;
+        
+        int rowStart = 0,colStart = 0, rowLast = row - 1, colLast = col - 1;
+        
+        while(ans.size() < totalDigit){
+            for(int i = colStart; i <= colLast && ans.size() < totalDigit; i++) ans.push_back(matrix[rowStart][i]);
+            rowStart++;
+            for(int i = rowStart; i <= rowLast && ans.size() < totalDigit; i++) ans.push_back(matrix[i][colLast]);
+            colLast--;
+            for(int i = colLast; i >= colStart && ans.size() < totalDigit; i--) ans.push_back(matrix[rowLast][i]);
+            rowLast--;
+            for(int i = rowLast; i >= rowStart && ans.size() < totalDigit; i--) ans.push_back(matrix[i][colStart]);
+            colStart++;
+        }
+        return ans;
+    }
+};
