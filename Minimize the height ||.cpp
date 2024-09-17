9f9d82f184d38ee87f8d3f3950d1cacc2918bb00
Bambam Thakur <131291624+Shivam-thakur05@users.noreply.github.com>
2024-09-17 23:39:16 +0530

    Minimize the height ||

---
+++ Minimize the height ||.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int getMinDiff(vector<int> &arr, int k) {
+        int n = arr.size();
+        if(n == 1) return 0;
+        sort(arr.begin(),arr.end());
+        int diff = arr[n-1] - arr[0];
+        int mini,maxi;
+        for(int i = 1; i < n; i++){
+            if(arr[i]-k < 0) continue;
+            maxi = max(arr[i-1]+k, arr[n-1]-k);
+            mini = min(arr[0]+k, arr[i]-k);
+            diff = min(diff, maxi-mini);
+        }
+        return diff;
+    }
+};
